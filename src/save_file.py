aea210d4be164b694ac57b42ba1b5d8336f93d1f
Wizard <merihov2yandex.ru>
2026-01-13 18:53:59 +0300

    update save_file.py

--- src/save_file.py
+++ src/save_file.py
@@ -6,26 +6,20 @@ class AbstractWorkFile(ABC):
     """Абстрактный класс для работы с файлами"""
 
     @abstractmethod
-    def add_vacancy(self):
+    def add_vacancy(self, vacancy, file_name):
         """Абстрактный метод для добавления вакансий"""
 
         pass
 
     @abstractmethod
-    def get_data(self):
+    def get_data(self, file_name, keyword):
         """Абстрактный метод для получения данных"""
 
         pass
 
     @abstractmethod
-    def del_info(self):
+    def del_info(self, file_name, name):
         """Абстрактный метод для удаления информации"""
-
-        pass
-
-    @abstractmethod
-    def save_file(self, vacancy, file_name):
-        """Абстрактный метод для сохранения информации"""
 
         pass
 
@@ -33,23 +27,35 @@ class AbstractWorkFile(ABC):
 class SaveFile(AbstractWorkFile):
     """Класс для сохранения информации о вакансиях"""
 
-    def add_vacancy(self):
+    def __init__(self, file_name='vacancy.json'):
+        self.__file_name = file_name
+
+    def add_vacancy(self, vacancy, file_name):
         """Метод для добавления вакансий"""
-        pass
-
-    def get_data(self):
-        """Метод для получения данных"""
-
-        pass
-
-    def del_info(self):
-        """Метод для удаления информации"""
-
-        pass
-
-    def save_file(self, vacancy, file_name):
-        """Метод для сохранения информации"""
 
         data = vacancy
-        with open('vacancy.json', 'a', encoding="UTF-8") as f:
+        with open(file_name, 'a', encoding="UTF-8") as f:
             json.dump(data, f)
+
+    def get_data(self, file_name, keyword):
+        """Метод для получения данных"""
+
+        with open(file_name, 'r', encoding='UTF-8') as f:
+            data = json.load(f)
+        if "items" in data:
+            return [item for item in data["items"] if keyword in item]
+        else:
+            return []
+
+    def del_info(self, file_name, name):
+        """Метод для удаления информации"""
+
+        with open(file_name, 'r', encoding='UTF-8') as f:
+            data = json.load(f)
+        data["items"] = [item for item in data["items"] if item["name"] != name]
+        with open(file_name, 'w', encoding='UTF-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+
+
